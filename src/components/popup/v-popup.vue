29653db0284db82026e276e992ce8c7f508d473f
Dmytro <dmytroivanovichn@gmail.com>
2022-06-01 20:10:00 +0300

    create popup

---
+++ src/components/popup/v-popup.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="v-popup">
+      <div class="v-popup__header">
+
+      </div>
+      <div class="v-popup__content">
+
+      </div>
+      <div class="v-popup__footer">
+         
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name:'v-popup',
+   props:{
+
+   },
+   data:()=>({
+
+   }),
+   computed:{
+
+   },
+   methods:{
+
+   }
+}
+</script>
+
+<style lang="less">
+
+</style>
